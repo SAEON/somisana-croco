8699215c1f2f1bc1e3aeaeef25d2ee79e77933af
GilesFearon <gfearon11@gmail.com>
2024-06-10 10:15:37 +0200

    add algoa_01 as an operational domain

---
+++ configs/algoa_01/croco_v1.3.1/myenv_frcst.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the environment variables for a forecast run
+# just putting in the variables which we may want to make configurable
+# A lot of variables in myenv_inter.sh aren't here as they are already defined as part of the operational workflow
+
+# provide the path to the croco source code
+export SOURCE=/croco-v1.3.1/OCEAN/
+
+# MPI partitioning
+export MPI_NUM_X=4
+export MPI_NUM_Y=3
+export MPI_NUM_PROCS=$(($MPI_NUM_X * $MPI_NUM_Y))
