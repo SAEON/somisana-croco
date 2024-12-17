b373ff6870a96ecf07a0d9d86e0e72b29492596d
GilesFearon <gfearon11@gmail.com>
2024-12-17 12:18:44 +0200

    add tides to operational workflow

---
+++ configs/sa_southeast_01/croco_v1.3.1/TPXO10/make_tides_inter.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python /home/gfearon/code/somisana-croco/cli.py make_tides_inter \
+	--input_dir /home/gfearon/code/somisana-croco/DATASETS_CROCOTOOLS/TPXO10/ \
+	--output_dir /home/gfearon/code/somisana-croco/configs/sa_southeast_01/croco_v1.3.1/TPXO10 \
+	--month_start 2009-01 \
+	--month_end 2009-12 \
+	--Yorig 1993
+
