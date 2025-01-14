07b3f4b360af3a025733a85a3356df6dd8c7469c
Giles Fearon <gfearon11@gmail.com>
2025-01-14 08:49:54 +0200

    update eez config

--- configs/sa_eez_01/croco_v1.3.1/GLORYS/make_bry_inter.bash
+++ configs/sa_eez_01/croco_v1.3.1/GLORYS/make_bry_inter.bash
@@ -3,7 +3,7 @@
 python /home/gfearon/code/somisana-croco/cli.py make_bry_inter \
 	--input_dir /home/gfearon/code/somisana-croco/DATASETS_CROCOTOOLS/GLORYS/eez \
 	--output_dir $PWD \
-	--month_start 2009-01 \
-	--month_end 2009-12 \
-	--Yorig 1990
+	--month_start 2008-01 \
+	--month_end 2013-12 \
+	--Yorig 1993
 
