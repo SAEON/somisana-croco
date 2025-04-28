ae413f6aaa9fb6276fbbf2e415f7ee75b6f0de8c
Giles Fearon <gfearon11@gmail.com>
2025-04-28 13:14:04 +0200

    update config

--- configs/test_02/croco_v1.3.1/TPXO10/make_tides_inter.bash
+++ configs/test_02/croco_v1.3.1/TPXO10/make_tides_inter.bash
@@ -3,7 +3,7 @@
 python /home/gfearon/code/somisana-croco/cli.py make_tides_inter \
 	--input_dir /home/gfearon/code/somisana-croco/DATASETS_CROCOTOOLS/TPXO10/ \
 	--output_dir /home/gfearon/code/somisana-croco/configs/test_02/croco_v1.3.1/TPXO10 \
-	--month_start 2022-01 \
-	--month_end 2024-01 \
+	--month_start 2015-01 \
+	--month_end 2021-12 \
 	--Yorig 1993
 
