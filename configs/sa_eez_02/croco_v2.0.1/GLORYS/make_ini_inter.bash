b7b54297a2fc264a53ceb2ed57413e7aba0374f3
Giles Fearon <gfearon11@gmail.com>
2025-06-08 21:24:42 +0200

    add GRID to new config

---
+++ configs/sa_eez_02/croco_v2.0.1/GLORYS/make_ini_inter.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python /home/gfearon/code/somisana-croco/cli.py make_ini_inter \
+	--input_dir /home/gfearon/code/somisana-croco/DATASETS_CROCOTOOLS/GLORYS/eez \
+	--output_dir $PWD \
+	--month_start 2008-01 \
+	--Yorig 1993
+
